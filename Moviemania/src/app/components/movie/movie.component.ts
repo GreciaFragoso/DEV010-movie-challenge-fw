1b531056cfa77cd85a8bf997c3e69e84159b4372
Grecia Fragoso <naomistereo96@gmail.com>
2023-10-18 11:33:05 -0500

    changelog file added

--- Moviemania/src/app/components/movie/movie.component.ts
+++ Moviemania/src/app/components/movie/movie.component.ts
@@ -3,13 +3,13 @@ import { Movie } from './../../models/movie.model';
 
 @Component({
   selector: 'app-movie', // se usa para llamarlo en el contenedor padre
-  templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  templateUrl: './movie.component.html', // html correspondiente
+  styleUrls: ['./movie.component.css'] // archivo CSS correspondiente
 })
-export class MovieComponent {
-
-  @Input() movie: Movie = { // movie!: Movie; por si no lo encuentra y no quiero inicializar
-    id: 0,
+export class MovieComponent { // exportamos para usarlo
+// input es un decorador que sirve para pasar datos de un componente padre a un hijo
+  @Input() movie: Movie = { //  puedo usar 'movie!: Movie;' por si no lo encuentra y no quiero inicializar
+    id: 0, // incializo valores default
     title: 'The Neon Demon',
     genre: 'Thriller',
   }; // inicializar es buena práctica para renderzar por defecto
